797b6d37612dbe5635a80fbe93c1515c3264425f
Sickcop <juan,hurtado2@ucp.edu.co>
2024-07-24 12:51:21 -0500

    changing info

--- project_3D_developer_portfolio/src/constants/index.js
+++ project_3D_developer_portfolio/src/constants/index.js
@@ -112,32 +112,7 @@ const technologies = [
 ];
 
 const experiences = [
-  {
-    title: "React.js Developer",
-    company_name: "Esinergia",
-    
-    iconBg: "#383E56",
-    date: "March 2022 - April 2023",
-    points: [
-      "Developing and maintaining web applications using React.js and other related technologies.",
-      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
-      "Implementing responsive design and ensuring cross-browser compatibility.",
-      "Participating in code reviews and providing constructive feedback to other developers.",
-    ],
-  },
-  {
-    title: "React Native Developer",
-    company_name: "Redstro start-up",
-    
-    iconBg: "#E6DEDD",
-    date: "Jan 2022 - Feb 2023",
-    points: [
-      "Developing and maintaining web applications using React.js and other related technologies.",
-      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
-      "Implementing responsive design and ensuring cross-browser compatibility.",
-      "Participating in code reviews and providing constructive feedback to other developers.",
-    ],
-  },
+
   {
     title: "Full stack Developer",
     company_name: "Free-lance",
